792590ad6d2e64ed5c021750ea96b7f66b7b78c7
nadamansor <nadamn94@gmail.com>
2019-02-09 13:40:50 +0200

    renaming variables of landing window and signin window

--- src/client/Client.java
+++ src/client/Client.java
@@ -16,8 +16,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 //import javafx.scene.layout.Background;
 //import javafx.scene.layout.BackgroundImage;
 //import javafx.scene.layout.BackgroundPosition;
@@ -35,104 +33,107 @@ import javafx.stage.Stage;
 
 public class Client extends Application implements EventHandler<ActionEvent> {
 
-   Button btn1;
-   Button btn2;
-   Scene scene1;
-   Scene scene2;
-    Label laSceneUser;
-    TextField tfName;
-    Label laScenePass;
-    PasswordField pfPwd;
-    GridPane grid2;
-   GridPane grid1;
-   BorderPane bp;
-   Button btnSubmit;
-    Button btnClear;
-    Button btnBack;
+    Button signUpButton;
+    Button signInButton;
+    Scene landingWindowScene;
+    Scene signInScene;
+    Label usernameLabel;
+    TextField userameTextFld;
+    Label passwordLabel;
+    PasswordField passwordFld;
+    GridPane SignInGridPane;
+    GridPane landingWindowGridPane;
+    BorderPane signInWindowBorderPane;
+    Button signInSubmitButton;
+    Button signInClearButton;
+    Button signInBackButton;
     HBox hbButtons;
-    Scene Xscene;
-   Stage ps;
+    Scene currentScene;
+    Stage ps;
+
     @Override
-    public void start(Stage primaryStage) throws FileNotFoundException  {
-       ps=primaryStage;
-       signWinInit();
-       formWinInit();
-       primaryStage.setTitle("TicTacToe");
-        ps.setScene(Xscene);
+    public void start(Stage primaryStage) throws FileNotFoundException {
+        ps = primaryStage;
+        landingWinInit();
+        signInWinInit();
+        primaryStage.setTitle("TicTacToe");
+        ps.setScene(currentScene);
         ps.show();
-    
-    }   
-    public void signWinInit(){
-       btn1 =new Button("Sign Up");
-        btn1.setOnAction((EventHandler<ActionEvent>) this);
-        
-        btn2 = new Button("Sign In");
-        
-        btn2.setOnAction((EventHandler<ActionEvent>)this);
-        grid1 = new GridPane();
-        grid1.setId("grid1");
-        grid1.setAlignment(Pos.CENTER);
-        grid1.setHgap(5);
-        grid1.setVgap(5);
-        grid1.setPadding(new Insets(5,5,5,5));
-        
-        grid1.add(btn1,0,3);
-        grid1.add(btn2,4,3);
-        scene1 = new Scene(grid1,600,600);
-        scene1.getStylesheets().add(Client.class.getResource("tictaktoe.css").toExternalForm());
-        Xscene=scene1;
+
+    }
+
+    public void landingWinInit() {
+        signUpButton = new Button("Sign Up");
+        signUpButton.setOnAction((EventHandler<ActionEvent>) this);
+
+        signInButton = new Button("Sign In");
+
+        signInButton.setOnAction((EventHandler<ActionEvent>) this);
+        landingWindowGridPane = new GridPane();
+        landingWindowGridPane.setId("grid1");
+        landingWindowGridPane.setAlignment(Pos.CENTER);
+        landingWindowGridPane.setHgap(5);
+        landingWindowGridPane.setVgap(5);
+        landingWindowGridPane.setPadding(new Insets(5, 5, 5, 5));
+
+        landingWindowGridPane.add(signUpButton, 0, 3);
+        landingWindowGridPane.add(signInButton, 4, 3);
+        landingWindowScene = new Scene(landingWindowGridPane, 600, 600);
+        landingWindowScene.getStylesheets().add(Client.class.getResource("tictaktoe.css").toExternalForm());
+        currentScene = landingWindowScene;
     }
 ////////////////////////////////////////////////////////////////////////////////////////////////
-    public void formWinInit(){
-            laSceneUser=new Label("User Name");
-        laScenePass=new Label("Password");
-        tfName = new TextField();
-        pfPwd = new PasswordField();
-        btnSubmit = new Button("Submit");
-        
-        btnSubmit.setOnAction((EventHandler<ActionEvent>)this);
-        btnClear = new Button("Clear");
-        btnClear.setOnAction((EventHandler<ActionEvent>) this);
-        btnBack = new Button("Back");
-        btnBack.setOnAction((EventHandler<ActionEvent>) this);
+
+    public void signInWinInit() {
+        usernameLabel = new Label("User Name");
+        passwordLabel = new Label("Password");
+        userameTextFld = new TextField();
+        passwordFld = new PasswordField();
+        signInSubmitButton = new Button("Submit");
+
+        signInSubmitButton.setOnAction((EventHandler<ActionEvent>) this);
+        signInClearButton = new Button("Clear");
+        signInClearButton.setOnAction((EventHandler<ActionEvent>) this);
+        signInBackButton = new Button("Back");
+        signInBackButton.setOnAction((EventHandler<ActionEvent>) this);
         hbButtons = new HBox();
         hbButtons.setSpacing(10.0);
-        hbButtons.getChildren().addAll(btnSubmit, btnClear, btnBack);
-        bp=new BorderPane();
-       ////////////////
-        grid2= new GridPane();
-        grid2.setAlignment(Pos.CENTER);
-        grid2.setVgap(12);
-        grid2.add(laSceneUser, 0, 0);
-        grid2.add(tfName, 1, 0);
-        grid2.add(laScenePass, 0, 1);
-        grid2.add(pfPwd, 1, 1);
-        grid2.add(hbButtons, 0, 2, 2, 1);
-        scene2=new Scene(grid2,600,600);
+        hbButtons.getChildren().addAll(signInSubmitButton, signInClearButton, signInBackButton);
+        signInWindowBorderPane = new BorderPane();
+        ////////////////
+        SignInGridPane = new GridPane();
+        SignInGridPane.setAlignment(Pos.CENTER);
+        SignInGridPane.setVgap(12);
+        SignInGridPane.add(usernameLabel, 0, 0);
+        SignInGridPane.add(userameTextFld, 1, 0);
+        SignInGridPane.add(passwordLabel, 0, 1);
+        SignInGridPane.add(passwordFld, 1, 1);
+        SignInGridPane.add(hbButtons, 0, 2, 2, 1);
+        signInScene = new Scene(SignInGridPane, 600, 600);
     }
-    
-  public static void main(String[] args) {
+
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void handle(ActionEvent e) {
-       if(e.getSource()==btnSubmit){
-           /**code here**/
-           System.out.println("Submit");}
-       
-       else if(e.getSource()==btnBack){
-            ps.setScene(scene1);
-       }
-       else if(e.getSource()==btnClear){ tfName.setText(""); pfPwd.setText("");}
-       else if(e.getSource()==btn1){
-           ps.setScene(scene2);
-       }
-       else if(e.getSource()==btn2){
-          ps.setScene(scene2);
-     }
-       
-        
+        if (e.getSource() == signInSubmitButton) {
+            /**
+             * code here*
+             */
+            System.out.println("Submit");
+        } else if (e.getSource() == signInBackButton) {
+            ps.setScene(landingWindowScene);
+        } else if (e.getSource() == signInClearButton) {
+            userameTextFld.setText("");
+            passwordFld.setText("");
+        } else if (e.getSource() == signUpButton) {
+            ps.setScene(signInScene);
+        } else if (e.getSource() == signInButton) {
+            ps.setScene(signInScene);
+        }
+
     }
 
 }
